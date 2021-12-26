adc82ee725f02d954f317cf0161c4f38aea7b3b1
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2021-12-26 17:59:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ Webcam.set ({
 });
 camera = document.getElementById("camera");
 Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='" + data_uri + "' />"
+    });
+}
+console.log("ml5 version - " + ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/V0gqFrr05/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("model is loaded");
+}
