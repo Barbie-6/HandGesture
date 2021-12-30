1dd29f9f5d1ad64cb4dcb740378ce3b9cc1f0d35
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2021-12-30 21:02:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,40 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log("model is loaded");
 }
+
+function Speak() {
+    synth = window.speechSynthesis;
+    speech_data = "The  Prediction Is - " + Prediction;
+    utterthis = new SpeechSynthesisUtterance(speech_data);
+    synth.speak(utterthis);
+}
+
+function Check_image() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        Prediction = results[0].label;
+
+        document.getElementById("result_gesture").innerHTML = Prediction;
+        Speak();
+
+        if (Prediction == "Good Job") {
+            document.getElementById("Prediction_emoji").innerHTML = "&#128077;";
+        }
+        if (Prediction == "Nice") {
+            document.getElementById("Prediction_emoji").innerHTML = "&#128076";
+        }
+        if (Prediction == "Victory") {
+            document.getElementById("Prediction_emoji").innerHTML = "&#9996;";
+        }
+
+        
+    }
+}
